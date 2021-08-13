38c7a3b9ce9a0124619b41b001744a00c1479e85
Matt Phillips <mattcphillips17@gmail.com>
2021-08-13 12:36:52 -0700

    basic server/router wiring setup

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,14 @@
-// build your `/api/resources` router here
+const express = require('express')
+const helpers = require('./model')
+
+const router = express.Router()
+
+router.post('/', (req, res, next) => {
+
+})
+
+router.get('/', (req, res, next) => {
+
+})
+
+module.exports = router
